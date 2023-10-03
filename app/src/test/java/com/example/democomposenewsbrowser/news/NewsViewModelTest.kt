dcba9c3829e4bccbc06f27278a38a109bd56b3a1
Wayne <w702897028970289@gmail.com>
2023-10-03 17:39:26 +0800

    NewsViewModelTest 基本setup

--- app/src/test/java/com/example/democomposenewsbrowser/news/NewsViewModelTest.kt
+++ app/src/test/java/com/example/democomposenewsbrowser/news/NewsViewModelTest.kt
@@ -1,4 +1,26 @@
 package com.example.democomposenewsbrowser.news
 
+import com.example.democomposenewsbrowser.ui.news.NewsViewModel
+import com.example.shared_test.newsbrowser.FakeNewsRepository
+import com.example.shared_test.newsbrowser.MainCoroutineRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+
 class NewsViewModelTest {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    private lateinit var newsViewModel: NewsViewModel
+
+    private lateinit var newsRepository: FakeNewsRepository
+
+    @Before
+    fun setupNewsViewModel() {
+        newsRepository = FakeNewsRepository()
+        newsViewModel = NewsViewModel(newsRepository)
+    }
+
 }
