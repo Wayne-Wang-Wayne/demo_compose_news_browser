71feac8c91f5e0441ddf6c3c77ce90e59f15c320
Wayne <w702897028970289@gmail.com>
2023-10-05 16:16:43 +0800

    將drawer按鈕的contentDescription改為Open Drawer並extract成string resource

--- app/src/main/java/com/example/democomposenewsbrowser/ui/util/TopAppBar.kt
+++ app/src/main/java/com/example/democomposenewsbrowser/ui/util/TopAppBar.kt
@@ -6,6 +6,8 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.democomposenewsbrowser.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -35,7 +37,7 @@ fun DrawerIcon(
     modifier: Modifier = Modifier
 ) {
     Icon(
-        Icons.Default.Menu, contentDescription = "drawer",
+        Icons.Default.Menu, contentDescription = stringResource(R.string.open_drawer),
         modifier = modifier
     )
 }
